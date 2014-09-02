58967e16e02ddb1a98c937bd32b80ce300a1c76c
WonderBeat <borov.htid@gmail.com>
2014-09-02 22:21:34 +0400

    Stub updated to adapt to runDB

--- Handler/Race.hs
+++ Handler/Race.hs
@@ -6,7 +6,6 @@ import Import
 getRaceR :: Handler TypedContent
 getRaceR = do
     track <- runDB $ getBy $ UniqueMapTitle "First"
-    selectRep $ do
-        provideRep $ return $ object [ "title" .= title ]
-    where
-      title = "a" :: Text
+    case track of
+        Nothing -> selectRep $ do provideRep $ return ("No such track" :: Text)
+        Just(Entity _ thisMap) -> selectRep $ do provideRep $ return $ object [ "title" .= (mapTitle thisMap) ]
