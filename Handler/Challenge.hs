e919d411a4374c7a1d6eb33e0e791207a2851000
WonderBeat <borov.htid@gmail.com>
2014-09-22 17:57:51 +0400

    Map view

--- Handler/Challenge.hs
+++ Handler/Challenge.hs
@@ -1,41 +1,63 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TupleSections     #-}
 {-# LANGUAGE TemplateHaskell   #-}
+{-# LANGUAGE PatternGuards #-}
 module Handler.Challenge(ChallengeR(..), postAttemptR, getChallengeByIdR) where
 
 import           Import
 import Network.HTTP.Types.Status
 import Data.Int(Int64)
 import App.UTCTimeP(UTCTimeP(..))
+import Data.List(head)
+import           Data.Time.Clock.POSIX
 
-getChallengeByIdR :: Int -> UTCTimeP -> Handler Value
+getChallengeByIdR :: Int -> UTCTimeP -> Handler TypedContent
 getChallengeByIdR user time' = do
      (Entity _ challenge) <- runDB $ getBy404 $ UnicUserTime (fromIntegral user) (unUTCTimeP time')
      track <- runDB $ get404 $ (challengeTrack challenge)
      let Key (PersistInt64 trackId') = challengeTrack challenge
-     let challengeR = (ChallengeR <$> (round . challengeUser) <*> (const trackId') <*> UTCTimeP . challengeTime) challenge
-     returnJson $ ChallengeResponse challengeR (trackCheckpoints track)
+     let challengeR = (ChallengeR <$> (round . challengeUser) <*> (const trackId') <*> (map UTCTimeP) . challengeTime) challenge
+     selectRep $ do
+        provideRep $ returnJson $ ChallengeResponse challengeR (trackCheckpoints track)
+        provideRep $ defaultLayout $ challengeWidget
+
+challengeWidget :: Widget
+challengeWidget = do
+  setTitleI MsgChallengeInfo
+  addStylesheet $ StaticR css_bootstrap_min_css
+  addScriptRemote "//cdn.jsdelivr.net/underscorejs/1.7.0/underscore-min.js"
+  addScriptRemote "//maps.googleapis.com/maps/api/js?key=AIzaSyAzkMb90k70kJUzgFmzk5xMbbtA7ZvT9g0&sensor=false"
+  $(widgetFile "challenge")
 
 postAttemptR :: Handler Value
 postAttemptR = do
     challenge <- requireJsonBody
     --let userKey = Key $ PersistInt64 (fromIntegral $ userId challenge)
     let userKey = (fromInteger . toInteger . userId) challenge :: Double
-    let trackKey = Key $ PersistInt64 (fromIntegral $ trackId challenge)
     (user, track) <- runDB $ do
         --user <- get userKey
         let user = Just userKey
-        track <- get trackKey
+        track <- getBy $ (UnicTrackDate . posixSecondsToUTCTime . fromIntegral . trackId) challenge
         return (user, track)
     _ <- case (user, track) of
-            (Just _, Just _) -> runDB $ insert $ Challenge userKey trackKey $ unUTCTimeP (time challenge)
+            (Just _, Just(Entity trackKey trackData))
+                | isMonotonousTimeSeq (time challenge)
+                , sameLength (trackCheckpoints trackData) (time challenge)
+                ->runDB $ insert $ (Challenge userKey trackKey <$> unUTCTimeP . head . time <*> (map unUTCTimeP) . time) challenge
             _ -> sendResponseStatus status400 ()
     sendResponseStatus status204 ()
+    where
+        isMonotonousTimeSeq :: [UTCTimeP] -> Bool
+        isMonotonousTimeSeq (a:b:xs) | (a > b) = False
+                                     | otherwise = isMonotonousTimeSeq (b:xs)
+        isMonotonousTimeSeq (_:[]) = True
+        isMonotonousTimeSeq [] = True
+        sameLength = \x y -> (length x) == (length y)
 
 data ChallengeR = ChallengeR {
     userId:: Int64,
     trackId:: Int64,
-    time:: UTCTimeP
+    time:: [UTCTimeP]
 } deriving (Eq, Show)
 
 data ChallengeResponse = ChallengeResponse ChallengeR [LatLng] deriving (Show)
--- Handler/Home.hs
+++ Handler/Home.hs
@@ -5,6 +5,7 @@ import Import
 
 import App.Pieces
 import App.UTCTimeP
+import Data.Text as T
 
 -- This is a handler function for the GET request method on the HomeR
 -- resource pattern. All of your resource patterns are defined in
